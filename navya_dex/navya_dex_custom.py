44372aebf29607ca07f76c4a22e3813a7a959f06
smehata <smehata@dexciss.com>
2021-01-02 20:21:43 +0530

    updated

--- navya_dex/navya_dex_custom.py
+++ navya_dex/navya_dex_custom.py
@@ -7,7 +7,7 @@ def add_employee_in_task(doc, method):
         query = """select tptt.employee, tptt.employee_name from `tabProject Template Task` as tptt
             inner join `tabProject Template` as tpt on tpt.name = tptt.parent
             inner join `tabProject` as tp on tp.project_template = tpt.name
-            where tp.name = '{0}' limit 1""".format(doc.project)
+            where tp.name = '{0}' and subject = '{1}' limit 1""".format(doc.project,doc.subject)
         reslt = frappe.db.sql(query,as_dict=True)
         if reslt:
             doc.employee = reslt[0].employee
