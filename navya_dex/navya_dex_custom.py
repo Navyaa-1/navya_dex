411304b3e6c1803524aba939a5e9130d5125ee1a
smehata <smehata@dexciss.com>
2021-01-21 13:41:11 +0530

    updated

--- navya_dex/navya_dex_custom.py
+++ navya_dex/navya_dex_custom.py
@@ -3,17 +3,17 @@ from __future__ import unicode_literals
 import frappe
 from frappe import _
 
-# def add_employee_in_task(doc, method):
-#     if doc.project:
-#         query = """select tptt.employee, tptt.employee_name from `tabProject Template Task` as tptt
-#             inner join `tabProject Template` as tpt on tpt.name = tptt.parent
-#             inner join `tabProject` as tp on tp.project_template = tpt.name
-#             where tp.name = '{0}' and subject = '{1}' limit 1""".format(doc.project,doc.subject)
-#         reslt = frappe.db.sql(query,as_dict=True)
-#         if reslt:
-#             doc.employee = reslt[0].employee
-#             doc.employee_name = reslt[0].employee_name
-#             doc.db_update()
+def add_employee_in_task(doc, method):
+    if doc.project:
+        query = """select tptt.employee, tptt.employee_name from `tabProject Template Task` as tptt
+            inner join `tabProject Template` as tpt on tpt.name = tptt.parent
+            inner join `tabProject` as tp on tp.project_template = tpt.name
+            where tp.name = '{0}' and subject = '{1}' limit 1""".format(doc.project,doc.subject)
+        reslt = frappe.db.sql(query,as_dict=True)
+        if reslt:
+            doc.employee = reslt[0].employee
+            doc.employee_name = reslt[0].employee_name
+            doc.db_update()
 
 def validate_working_status(doc, method):
     if doc.get("__islocal"):
